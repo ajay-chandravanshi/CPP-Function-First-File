5a17d6e3d6348ba03c952a8352dd43881db9ac12
ajay-chandravanshi <ajaystudy38@gmail.com>
2024-12-18 01:49:45 +0530

    first commit

---
+++ 3.Call the function(Example-3).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void display()
+{
+    cout<<"Hello Sir\n";
+    cout<<"I Hope You are Fine\n";
+    cout<<"This is a Survay About You\n";
+    cout<<"Have a nice day";
+}
+ 
+int main()
+{
+
+display();
+}
---
+++ 4.Call Two Functiuons(example-4).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+void display()
+{
+    cout<<"Hello Sir\n";
+    cout<<"I Hope You are Fine\n";
+    cout<<"This is a Survay About You\n";
+    cout<<"Have a nice day\n\n";
+}
+ void dish()
+ {
+    cout<<"This is the another file\n";
+    cout<<"My name is ajay\n";
+    cout<<"I am from Bhopal\n";
+    cout<<"Nice to meet You";
+ }
+int main()
+{
+
+display();
+dish();
+}
